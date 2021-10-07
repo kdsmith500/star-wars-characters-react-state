c5de819629497abc81f01b061b2de6b5bc4f7b7b
Kyle Smith <k.d.smith500@gmail.com>
2021-10-07 17:09:22 -0500

    sets up useEffect to fetch a list of characters from the starwars api, sets up state to keep track of fetch and show user status loading etc, refactors to useReducer to manage the state of the download with dispatch instead of throughout the component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
-
 import './styles.scss';
+import endpoint from './endpoint';
+
+const initialState = {
+  result: null,
+  loading: true,
+  error: null
+}
+
+const fetchReducer = (state, action) => {
+  if (action.type === 'LOADING') {
+    return {
+      result: null,
+      loading: true,
+      error: null
+    }
+  }
+
+  if (action.type === 'RESPONSE_COMPLETE') {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null
+    }
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      result: null,
+      loading: false,
+      error: action.payload.error
+    }
+  }
+
+  return state;
+}
+
+const useFetch = url => {
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  useEffect(() => { // cant be made async but can contain an async function to be called if using promises is a problem
+    dispatch({ type: 'LOADING' });
+
+    fetch(endpoint + '/characters')
+      .then(response => response.json())
+      .then(response => {
+        dispatch({ type: 'RESPONSE_COMPLETE', payload: { response } });
+      })
+      .catch(error => {
+        dispatch({ type: 'ERROR', payload: { error } });
+      });
+    }, []);
+
+    return [state.result, state.loading, state.error];
+}
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+  const characters = (response && response.characters) || []; // keeps code from breaking when null
 
   return (
     <div className="Application">
@@ -19,7 +72,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
